6fc2d8e2ae7ed59a2df741d2894db5197a21d747
Duy Tran <57513519+tranducduy7520@users.noreply.github.com>
2020-11-27 16:00:15 +0700

    Create GOLD_MINING.cpp

---
+++ Training5-DynamicPrograming/GOLD_MINING.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, L1, L2;
+int a[100010], mem[100010];
+int best;
+
+int GM(int i)
+{
+    if (i < L1)
+    {
+        mem[i] = a[i];
+        return mem[i];
+    }
+    for (int j = (i - L2 > 0) ? (i - L2) : 0; j <= i - L1; j++)
+        mem[i] = max(mem[i], mem[j] + a[i]);
+    return mem[i];
+}
+
+int main()
+{
+    cin >> n >> L1 >> L2;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mem[i] = -1;
+    }
+    for (int i = 0; i < n; i++)
+        best = max(best, GM(i));
+    cout << best;
+    return 0;
+}
